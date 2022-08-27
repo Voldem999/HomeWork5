51429be8bf2566544ec3ede69df23e24e1698765
Ilya201091 <ZiK201091@ya.ru>
2022-08-27 18:19:02 +0700

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-
+        //Задача 1
+        int[] days = new int[2];
+        days[0] = 1;
+        days[1] = 2;
+        days[2] = 3;
+        int day1 = days[0];
+        int day2 = days[1];
+        int day3 = days[2];
 
 
     }}
