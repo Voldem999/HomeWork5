8db286f7f7d2859d24acb03b725e56b1450ffd2c
Ilya201091 <ZiK201091@ya.ru>
2022-08-27 22:47:27 +0700

    выполнил ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,49 @@
 public class Main {
     public static void main(String[] args) {
         //Задача 1
-        int[] days = new int[2];
+
+        int[] days = new int[3];
         days[0] = 1;
         days[1] = 2;
         days[2] = 3;
         int day1 = days[0];
         int day2 = days[1];
         int day3 = days[2];
+
+        double[] liters = new double [] {1.57, 7.654, 9.986};
+        liters[0] = 1.57;
+        liters[1] = 7.654;
+        liters[2] = 9.986;
+        double liter1 = liters[0];
+        double liter2 = liters[1];
+        double liter3 = liters[2];
+
+        int[] seasons = new int[4];
+        seasons[0] = 1;
+        seasons[1] = 2;
+        seasons[2] = 3;
+        seasons[3] = 4;
+        int season1 = seasons[0];
+        int season2 = seasons[1];
+        int season3 = seasons[2];
+        int season4 = seasons[3];
+
+        //Задача 2
+
+        for (int i = 0; i < days.length; i++) {
+            System.out.print(days[i] + ",");}
+        {System.out.println();}
+
+        for (int i = 0; i < liters.length; i++) {
+            System.out.print(liters[i] + ",");}
+        {System.out.println();}
+
+        for (int i = 0; i < seasons.length; i++) {
+            System.out.print(seasons[i] + ",");}
+        {System.out.println();}
+
+        //Задача 3
+
 
 
     }}
